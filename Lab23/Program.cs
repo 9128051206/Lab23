ba89d1722fc81b10fb31e9dd109ed93919eb959a
Юлия Шепелева <9128051206@mail.ru>
2022-02-06 16:44:32 +0500

    Добавьте файлы проекта.

---
+++ Lab23/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                int n = Convert.ToInt32(Console.ReadLine());
+                FactorialAsync(n);
+
+                Console.ReadKey();
+        }
+
+            static void Factorial(int n)
+            {                 
+               int factorial = 1;   
+
+                for (int i = 2; i <= n; i++) 
+                {
+                    factorial = factorial * i;
+                }
+                 Console.WriteLine(factorial);
+            }
+
+            static async void FactorialAsync(int n)
+            {
+                await Task.Run(() => Factorial(n));
+            }
+
+    }
+}
+    
+
